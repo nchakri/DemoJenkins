3da1f97b1addbe648ce4f273ba38f13d6d805d7d
nchakri <chakri.yo@gmail.com>
2018-03-05 16:40:15 +0530

    added code for addition of numbers

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,7 +4,10 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world!");
-		System.out.println("Hello world!");
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println("Output of 'a' plus 'b' is "+c);
 	}
 
 }

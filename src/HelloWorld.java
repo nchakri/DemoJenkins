ceb67eacd54d372e76a4ed9684e0d4125bc8826b
nchakri <chakri.yo@gmail.com>
2018-03-12 13:52:21 +0530

    updated code with input statements

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,6 +7,8 @@ public class HelloWorld {
 		int a=10;
 		int b=20;
 		int c=a+b;
+		System.out.println("Value of 'a' is "+a);
+		System.out.println("Value of 'b' is "+b);
 		System.out.println("Output of 'a' plus 'b' is "+c);
 	}
 
